3f1ccc097adc666844397ca65459571fde3d2e48
Roselette <shirocooper222@gmail.com>
2021-02-04 17:55:41 +0530

    .....
    ....

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Animator animator;
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetFloat("Horizontal", Input.GetAxisRaw("Horizontal"));
+        animator.SetFloat("Vertical", Input.GetAxisRaw("Vertical"));
+    }
+}
